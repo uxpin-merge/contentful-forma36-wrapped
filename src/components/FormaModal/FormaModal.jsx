bda475e9c07c2afd3d2af49be26c76a9007f6ddd
Jack Behar <jack@uxpin.com>
2021-06-24 17:23:50 -0600

    Modal: Started work on props/designer experience

--- src/components/FormaModal/FormaModal.jsx
+++ src/components/FormaModal/FormaModal.jsx
@@ -7,25 +7,58 @@ function FormaModal(props) {
   const [isShown, setShown] = React.useState(false);
   React.useEffect(() => setShown(props.isShown), [props]);
 
+  const {   hasCloseButton, closeButtonText, hasActionButton, actionButtonText, ...other } = props;
+
+
   return (
     <React.Fragment>
-      <Button onClick={() => setShown(true)}>{props.buttonText}</Button>
-      <Modal isShown={isShown}>
+      {/* <Button onClick={() => setShown(true)}>{props.buttonText}</Button> */}
+
+      {/* <Modal
+      modalContentProps={{
+        className: 'additional-modal-content-class'
+      }}
+      modalHeaderProps={{
+        className: 'additional-modal-header-class'
+      }}
+      onAfterOpen={function noRefCheck(){}}
+      onClose={() => setShown(false)}
+      title="Default modal"
+      isShown={isShown}
+    >
+      Modal content. It is centered by default.
+    </Modal> */}
+
+
+      <Modal isShown={isShown} >
         {() => (
           <React.Fragment>
-            <Modal.Header title={props.title} />
+            <Modal.Header title={props.title} onClose={() => setShown(false)}/>
             <Modal.Content>{props.content}</Modal.Content>
+            
+           
+ { (!hasActionButton && !hasCloseButton ) ? null : 
+
             <Modal.Controls>
-              <Button buttonType="positive" onClick={() => setShown(false)}>
-                Confirm
-              </Button>
-              <Button buttonType="muted" onClick={() => setShown(false)}>
-                Close
-              </Button>
+            
+            { 
+            // If hasActionButton = true, then display it
+            hasActionButton &&  
+            <Button buttonType="positive" onClick={props.onClick}>{actionButtonText}</Button>
+            }
+            { 
+            // If hasCloseButton = true, then display it
+            hasCloseButton &&  
+            <Button buttonType="muted" onClick={() => setShown(false)}>{closeButtonText}</Button>
+            }              
+
             </Modal.Controls>
+
+          }
+
           </React.Fragment>
         )}
-      </Modal>
+      </Modal> 
     </React.Fragment>
   );
 }
@@ -37,15 +70,22 @@ FormaModal.propTypes = {
   isShown: PropTypes.bool,
   content: PropTypes.string,
   shouldCloseOnEscapePress: PropTypes.bool,
+
+  hasCloseButton: PropTypes.bool,
+  closeButtonText: PropTypes.string,
+  hasActionButton:  PropTypes.bool,
+  actionButtonText: PropTypes.string,
   onClick: PropTypes.func,
-  buttonText: PropTypes.string,
 };
 
 FormaModal.defaultProps = {
   isShown: false,
   title: "Default modal",
-  buttonText: "Show modal",
   content: "Hello from controlled modal window",
+  hasCloseButton: true,
+  closeButtonText: "close",
+  hasActionButton:  true,
+  actionButtonText: "confirm",
 };
 
 export { FormaModal as default };
